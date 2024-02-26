91d58f7329d4512a078a6060ccf5981dc06b34eb
Sagar Roy <stackasgar@gmail.com>
2024-02-26 21:54:09 +0600

    removed unnecessary files

--- 0-server-builed/utils/error_res.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function error_res(res, error) {
-    res
-        .status(400)
-        .json({ message: typeof error === "string" ? error : error?.message });
-}
-exports.default = error_res;
--- 0-server-builed/utils/isTodayDate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isTodayDate(date) {
-    if (!date)
-        return false;
-    return new Date().toDateString() === new Date(date).toDateString();
-}
-exports.default = isTodayDate;
